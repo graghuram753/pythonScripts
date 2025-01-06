cb2b1fbca284e23b11ce2a60040aa70b35d9fbd2
Raghuram <graghuram753@outlook.com>
2025-01-07 00:46:43 +0530

    load initial file.

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("Hit")
+import pandas as pd
+import sys
+
+data = {'Name': ['Tom', 'nick', 'krish', 'jack'],
+        'Age': [20, 21, 19, 18]}
+df = pd.DataFrame(data)
+print(df)
+
+print ('argument list', sys.argv)
+first = int(sys.argv[1])
+second = int(sys.argv[2])
+print ("sum = {}".format(first+second))
